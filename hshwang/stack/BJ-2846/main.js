074cdc269d20a961567dba247fecfe2d702575d7
[아이엔소프트] 황호세 <hshwang@in-soft.co.kr>
2023-01-15 16:48:04 +0900

    chore: BJ-2846-오르막길(문제 주석추가)

--- hshwang/stack/BJ-2846/main.js
+++ hshwang/stack/BJ-2846/main.js
@@ -11,27 +11,34 @@ let saveAscent = [];
 let saveMaxNum = 0;
 
 const setMaxNum = (saveAscent) => {
+  //끝값에서 첫값을 뺀후 저장한 최대값과 비교.
   if(saveMaxNum < saveAscent[saveAscent.length - 1] - saveAscent[0]) {
     saveMaxNum = saveAscent[saveAscent.length - 1] - saveAscent[0];
   }
 }
 
 const finalMaxCheck = (arrayCount) => {
+  //ascentArr의 마지막에 도달했으나 배열이 있는상태라면?
   if(arrayCount === ascentArr.length - 1 && saveAscent.length >= 2) {
     setMaxNum(saveAscent);
-  } else if(ascentArr.length === 1 && saveAscent.length === 1) {
+  } 
+  //ascentArr 배열이 알고보니 길이가 1인 배열이였다면?
+  else if(ascentArr.length === 1 && saveAscent.length === 1) {
     saveMaxNum = saveAscent[0];
   }
 } 
 
 const ascentCheck = (saveAscent, ascentArr) => {
   for(let i = 0; i < ascentArr.length; i++) {
+    //배열의 맨처음이라면
     if(saveAscent.length === 0) {
       saveAscent.push(ascentArr[i])
     }
+    //이후 저장해둔 saveAscent의 끝값보다 ascentArr값이 크다면?
     else if (saveAscent[saveAscent.length - 1] < ascentArr[i]) {
       saveAscent.push(ascentArr[i])
     }
+    //그게아니라 저장해둔 saveAscent의끝값이 크다면?
     else if (saveAscent[saveAscent.length - 1] >= ascentArr[i]) {
       setMaxNum(saveAscent);
       saveAscent = [];
